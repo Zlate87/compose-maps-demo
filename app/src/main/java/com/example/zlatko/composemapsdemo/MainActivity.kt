fbe8556e821d1738c354813a80d41a39d7aba96c
Zlatko Stamatov <Zlatko@vw-dilab.com>
2021-12-08 18:16:33 +0100

    Removes not needed code.

--- app/src/main/java/com/example/zlatko/composemapsdemo/MainActivity.kt
+++ app/src/main/java/com/example/zlatko/composemapsdemo/MainActivity.kt
@@ -5,67 +5,41 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.zlatko.composemapsdemo.ui.theme.ComposeMapsDemoTheme
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppNavigation()
-        }
-    }
-
-    @Composable
-    fun AppNavigation() {
-        val navController = rememberNavController()
-        NavHost(navController = navController, startDestination = "Screen1") {
-            composable("Screen1") {
-                Button({ navController.navigate("MapsScreen") }) {
-                    Text(text = "Go to Maps")
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "Screen1") {
+                composable("Screen1") {
+                    Button({ navController.navigate("MapsScreen") }) {
+                        Text(text = "Go to Maps")
+                    }
                 }
-            }
-            composable("MapsScreen") {
-                val mapView = rememberMapViewWithLifecycle()
-                AndroidView({ mapView }) { mapView ->
-                    mapView.getMapAsync { map ->
-                        map.setOnInfoWindowClickListener {
-                            navController.navigate("Screen1")
-                        }
+                composable("MapsScreen") {
+                    val mapView = rememberMapViewWithLifecycle()
+                    AndroidView({ mapView }) { mapView ->
+                        mapView.getMapAsync { map ->
+                            map.setOnInfoWindowClickListener {
+                                navController.navigate("Screen1")
+                            }
 
-                        val markerOptions = MarkerOptions()
-                            .position(LatLng(41.390205, 2.154007))
-                            .title("Barcelona")
-                        map.addMarker(markerOptions)!!
+                            val markerOptions = MarkerOptions()
+                                .position(LatLng(41.390205, 2.154007))
+                                .title("Barcelona")
+                            map.addMarker(markerOptions)!!
+                        }
                     }
                 }
             }
-            composable("Screen2") {
-                Button({ navController.navigate("Screen1") }) {
-                    Text(text = "Go Home")
-                }
-
-            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeMapsDemoTheme {
-        Greeting("Android")
     }
 }
